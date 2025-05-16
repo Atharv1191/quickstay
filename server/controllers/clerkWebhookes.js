b564c9ec92b75e11415cba68a2695f355455f9a9
Atharv Joshi <atharvj07@gmail.com>
2025-05-16 10:23:13 +0530

    fix: rename userData field from 'name' to 'username' and initialize recentSearchedCities as an array

--- server/controllers/clerkWebhookes.js
+++ server/controllers/clerkWebhookes.js
@@ -18,9 +18,9 @@ const clerkWebhooks = async (req, res) => {
                 const userData = {
                     _id: data.id,
                     email: data.email_addresses[0]?.email_address || "",
-                    name: `${data.first_name || ""} ${data.last_name || ""}`,
+                    username: `${data.first_name || ""} ${data.last_name || ""}`,
                     image: data.image_url,
-                    recentSearchedCities: ""
+                    recentSearchedCities: []
                 };
                 await User.create(userData);
                 res.status(200).json({});
@@ -30,7 +30,7 @@ const clerkWebhooks = async (req, res) => {
             case 'user.updated': {
                 const userData = {
                     email: data.email_addresses[0]?.email_address || "",
-                    name: `${data.first_name || ""} ${data.last_name || ""}`,
+                    username: `${data.first_name || ""} ${data.last_name || ""}`,
                     image: data.image_url,
                 };
                 await User.findByIdAndUpdate(data.id, userData);
